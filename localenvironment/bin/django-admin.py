5ac7fc325814009da90ad5d688778bd18d877dcc
MihaiBorsu <mihai.borsu@gmail.com>
2020-06-02 22:46:56 +0300

    initial commit

---
+++ localenvironment/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mihai/Documents/UPT/3rdYearS2/DP/mysite/localenvironment/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
